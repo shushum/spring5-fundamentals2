3cc1e0188d16854bfd1309c8434f5b91e55c9e07
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-07-18 17:33:50 +0300

    lab - 3 done

--- src/main/java/lab/model/Person.java
+++ src/main/java/lab/model/Person.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface Person {
     void sayHello(Person person);
+
     int getId();
     String getName();
     Country getCountry();
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,16 +1,15 @@
-import lab.model.Person;
 import lab.model.SimpleCountry;
 import lab.model.UsualPerson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
-	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
+	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
 
 	private UsualPerson expectedPerson;
 
@@ -18,14 +17,13 @@ class HelloWorldTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+		context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
 		expectedPerson = getExpectedPerson();
 	}
 
 	@Test
 	void testInitPerson() {
-		Person person = context.getBean("person", Person.class);
-		assertEquals(expectedPerson, person);
+		assertEquals(expectedPerson, context.getBean("person"));
 //		System.out.println(person);
 	}
 
